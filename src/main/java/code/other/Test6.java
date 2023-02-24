54f04491412d76b0593f89051eb1f1d1a679abd5
gengyuan <2088034881@qq.com>
2023-02-24 20:12:27 +0800

    update

--- src/main/java/code/other/Test6.java
+++ src/main/java/code/other/Test6.java
@@ -8,23 +8,30 @@ import java.util.Set;
 public class Test6 {
     //单词拆分
     public static boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> word = new HashSet<>(wordDict);
-        boolean []dp = new boolean[s.length()+1];
+
+        Set<String> wordDictSet = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
-                if(dp[j] && word.contains(s.substring(j,i))){
+                // substring()
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
             }
         }
         return dp[s.length()];
+
+
+
+
+
     }
 
     public static void main(String[] args) {
-        List<String> dict = Arrays.asList("leet","code");
-        System.out.println(wordBreak("leetcode", dict));
+        List<String> dict = Arrays.asList("cats","dog","sand","and","cat");
+        System.out.println(wordBreak("catsandog", dict));
     }
 
 }
