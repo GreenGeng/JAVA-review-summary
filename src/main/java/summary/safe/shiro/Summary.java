d653a8acac99f1962fb5fa2a726ec608696f9af9
gengyuan <2088034881@qq.com>
2023-03-19 18:59:11 +0800

    添加shiro工具的使用

---
+++ src/main/java/summary/safe/shiro/Summary.java
@@ -0,0 +1,13 @@
+package summary.safe.shiro;
+
+public class Summary {
+    /**
+     * 使用shiro工具时候会报错
+     * 因为java 的版本太高了
+     * mac里给Java版本降级的方式
+     * 输入export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+     * 就行了
+     *
+     *
+     */
+}
